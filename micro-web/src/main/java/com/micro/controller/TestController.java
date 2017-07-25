522a1b411be63ef7adf2495b9b0aebe7ff1f171c
fdh <fdh@11.com>
2017-07-25 14:06:46 +0800

    初始化数据

--- micro-web/src/main/java/com/micro/controller/TestController.java
+++ micro-web/src/main/java/com/micro/controller/TestController.java
@@ -1,7 +1,12 @@
 package com.micro.controller;
 
+import com.dexcoder.dal.JdbcDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2017/7/24.
@@ -9,8 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TestController {
 
+    @Autowired
+    private JdbcDao jdbcDao;
+
     @RequestMapping("/test")
     public void test(){
-        System.out.println("hello");
+        List<Map<String, Object>> maps = jdbcDao.queryListForSql("select * from test");
+        System.out.println(maps);
     }
 }
